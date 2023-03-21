80e156bb3c4bc1a30bd03c7cd1d6a43114393a2c
yitang <yitang@netstore-sh.com>
2023-03-21 15:38:42 +0800

    ~

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -51,13 +51,12 @@ const actions = {
     return new Promise((resolve, reject) => {
       getInfo(state.token).then(response => {
         const { data } = response
-
         if (!data) {
           reject('Verification failed, please Login again.')
         }
 
         // const { roles, name, avatar, introduction } = data
-        const { roles, name, avatar, introduction } = data.data
+        const { roles, username, avatar, introduction } = data.data
 
         // roles must be a non-empty array
         if (!roles || roles.length <= 0) {
@@ -65,7 +64,7 @@ const actions = {
         }
 
         commit('SET_ROLES', roles)
-        commit('SET_NAME', name)
+        commit('SET_NAME', username)
         commit('SET_AVATAR', avatar)
         commit('SET_INTRODUCTION', introduction)
         resolve(data)
